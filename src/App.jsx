6efe6229be255f2f24f613386b6aef82cf1db229
Azim-Eham <azimeham84@gmail.com>
2025-06-17 23:38:21 +0600

    Fixed ui bug in mobile

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
   return (
     <Provider store={store}>
       <Router>
-        <div className="min-h-screen bg-white">
+        <div className="min-h-screen bg-white overflow-x-hidden">
           <Navbar />
           <main>
             <Hero />
